ffb368d7a681cfec0d65d52992407e21a2449b95
mauiroma <maurizio.romani@gmail.com>
2019-02-27 22:53:13 +0100

    fix pipelien

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -94,8 +94,11 @@ pipeline {
                     steps {
                         script {
                             openshift.withCluster('${OCP_CLUSTER}') {
-                                openshift.withProject('${OCP_PRJ_NAMESPACE}','ocp-meetup-token') {
+                                openshift.withProject('${OCP_PRJ_NAMESPACE}') {
                                     def bc = openshift.selector('bc', '${OCP_BUILD_NAME}')
+
+                                    echo bc
+
                                     bc.spec.output.to.kind.ImageStream.name = "${OCP_BUILD_NAME}:${BUILD_TAG}"
                                     openshift.apply(bc)
                                     /*
