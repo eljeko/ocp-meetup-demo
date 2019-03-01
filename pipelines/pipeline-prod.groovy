c887af10720e6236f58820456b6719c6281b13e9
mauiroma <maurizio.romani@gmail.com>
2019-03-01 13:49:34 +0100

    Fix type into pipeline

--- pipelines/pipeline-prod.groovy
+++ pipelines/pipeline-prod.groovy
@@ -110,7 +110,7 @@ pipeline {
                     withCredentials([string(credentialsId: "${OCP_SERVICE_TOKEN}", variable: 'OCP_SERVICE_TOKEN')]) {
                         def patchRoute =
                             sh(
-                                script: "oc patch route/${OCP_BUILD_NAME}  --patch='{"spec":{"to":{"name":"${OCP_BUILD_NAME}-${newState}"}}} --token=${OCP_SERVICE_TOKEN}  $target_cluster_flags",
+                                script: "oc patch route/${OCP_BUILD_NAME}  --patch='{\"spec\":{\"to\":{\"name\":\"${OCP_BUILD_NAME}-${newState}\"}}} --token=${OCP_SERVICE_TOKEN}  $target_cluster_flags",
                                 returnStdout: true
                             )
                         echo "Patched route: $patchRoute"
