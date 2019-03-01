62f90cef00c5238d65abe7adb0d2ce392c8f98aa
mauiroma <maurizio.romani@gmail.com>
2019-03-01 17:56:37 +0100

    Refactoring pipeline

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -14,7 +14,7 @@ pipeline {
                     }
                     echo "Releasing tag ${BUILD_TAG}"
                     target_cluster_flags = "--server=${OCP_CLUSTER_URL} --insecure-skip-tls-verify"
-                    target_cluster_flags = "$target_cluster_flags   --namespace=${OCP_PRJ_NAMESPACE}"
+                    target_cluster_flags = "$target_cluster_flags   --namespace=${OCP_PROJECT}"
                 }
             }
         }
@@ -29,21 +29,14 @@ pipeline {
                 )
             }
         }
-        stage('Maven') {
-            steps {
-                withMaven(mavenSettingsFilePath: "${MVN_SETTINGS}") {
-                    sh "mvn -f ${POM_FILE} versions:set -DnewVersion=${BUILD_TAG}"
-                }
-            }
-        }
-        stage('QA') {
+        stage('Quality Assurance') {
             parallel {
                 stage('SonarQube analysis') {
                     steps {
                         script{
                             if(Boolean.parseBoolean(env.SONAR)){
                                 withSonarQubeEnv('Sonar-MacLocalhost') {
-                                    withMaven(mavenSettingsFilePath: "${MVN_SETTINGS}") {
+                                    withMaven() {
                                         sh "mvn -f ${POM_FILE} sonar:sonar"
                                     }
                                 }
@@ -53,28 +46,21 @@ pipeline {
                         }
                     }
                 }
-                stage('Run Maven Tests') {
+                stage('Maven Tests') {
                     steps {
-                        withMaven(mavenSettingsFilePath: "${MVN_SETTINGS}") {
+                        withMaven() {
                             sh "mvn -f ${POM_FILE} test"
                         }
                     }
                 }
             }
         }
-        stage('Publish on nexus') {
+        stage('App Build and Package') {
             steps {
                 script{
-                    if(Boolean.parseBoolean(env.DEPLOY_ON_NEXUS)){
-                        echo "DEPLOY ON NEXUS"
-                        withMaven(mavenSettingsFilePath: "${MVN_SETTINGS}") {
-                            sh "mvn -f ${POM_FILE} clean deploy -Dmaven.javadoc.skip=true -DskipTests "
-                        }
-                    }else{
-                        echo "PACKAGE"
-                        withMaven(mavenSettingsFilePath: "${MVN_SETTINGS}") {
-                            sh "mvn -f ${POM_FILE} clean package -Dappversion=${BUILD_TAG} -Dmaven.javadoc.skip=true -DskipTests "
-                        }
+                    echo "PACKAGE"
+                    withMaven() {
+                        sh "mvn -f ${POM_FILE} clean package -Dappversion=${BUILD_TAG} -Dmaven.javadoc.skip=true -DskipTests "
                     }
                 }
             }
@@ -88,18 +74,16 @@ pipeline {
                             mkdir -p ${WORKSPACE}/s2i-binary
                             cp ${WORKSPACE}/web-app/target/ROOT.war ${WORKSPACE}/s2i-binary
                         """
-//                            mkdir -p ${WORKSPACE}/s2i-binary/configuration
-//                            cp ${WORKSPACE}/runtime-configuration/ocp/standalone-openshift.xml ${WORKSPACE}/s2i-binary/configuration/
                     }
                 }
-                stage('UpdateBuild') {
+                stage('Update BuildConfig') {
                     steps {
                         script {
                             withCredentials([string(credentialsId: "${OCP_SERVICE_TOKEN}", variable: 'OCP_SERVICE_TOKEN')]) {
                                 def buildconfigUpdateResult =
                                     sh(
-                                        script: "oc patch bc ${OCP_BUILD_NAME}  -p '{\"spec\":{\"output\":{\"to\":{\"kind\":\"ImageStreamTag\",\"name\":\"${OCP_BUILD_NAME}:${BUILD_TAG}\"}}}}' --token=${OCP_SERVICE_TOKEN} -o json $target_cluster_flags \
-                                                |oc replace ${OCP_BUILD_NAME} --token=${OCP_SERVICE_TOKEN} $target_cluster_flags -f -",
+                                        script: "oc patch bc ${OCP_APP_NAME}  -p '{\"spec\":{\"output\":{\"to\":{\"kind\":\"ImageStreamTag\",\"name\":\"${OCP_APP_NAME}:${BUILD_TAG}\"}}}}' --token=${OCP_SERVICE_TOKEN} -o json $target_cluster_flags \
+                                                |oc replace ${OCP_APP_NAME} --token=${OCP_SERVICE_TOKEN} $target_cluster_flags -f -",
                                         returnStdout: true
                                     )
                                 if (!buildconfigUpdateResult?.trim()) {
@@ -112,13 +96,13 @@ pipeline {
 
                     }
                 }
-                stage('StartBuild') {
+                stage('Start New Build') {
                     steps {
                         script {
                             withCredentials([string(credentialsId: "${OCP_SERVICE_TOKEN}", variable: 'OCP_SERVICE_TOKEN')]) {
                                 def startBuildResult =
                                     sh(
-                                        script: "oc start-build ${OCP_BUILD_NAME} --token=${OCP_SERVICE_TOKEN} --from-dir=${WORKSPACE}/s2i-binary $target_cluster_flags --follow",
+                                        script: "oc start-build ${OCP_APP_NAME} --token=${OCP_SERVICE_TOKEN} --from-dir=${WORKSPACE}/s2i-binary $target_cluster_flags --follow",
                                         returnStdout: true
                                     )
                                 if (!startBuildResult?.trim()) {
@@ -134,27 +118,27 @@ pipeline {
                     steps {
                         script {
                             withCredentials([string(credentialsId: "${OCP_SERVICE_TOKEN}", variable: 'OCP_SERVICE_TOKEN')]) {
-                                def patchIamgeStream =
+                                def patchImageStream =
                                     sh(
-                                        script: "oc set image dc/${OCP_BUILD_NAME} ${OCP_BUILD_NAME}=$docker_registry:5000/${OCP_PRJ_NAMESPACE}/${OCP_BUILD_NAME}:${BUILD_TAG} --token=${OCP_SERVICE_TOKEN} $target_cluster_flags",
+                                        script: "oc set image dc/${OCP_APP_NAME} ${OCP_APP_NAME}=$docker_registry:5000/${OCP_PROJECT}/${OCP_APP_NAME}:${BUILD_TAG} --token=${OCP_SERVICE_TOKEN} $target_cluster_flags",
                                         returnStdout: true
                                     )
                                 //If the output is true the image was the same, so we check if current image is really the desired version
-                                if (!patchIamgeStream?.trim()) {
+                                if (!patchImageStream?.trim()) {
                                     def currentImageStreamVersion =
                                         sh(
-                                            script: "oc get dc ${OCP_BUILD_NAME} -o jsonpath='{.spec.template.spec.containers[0].image}' --token=${OCP_SERVICE_TOKEN} $target_cluster_flags",
+                                            script: "oc get dc ${OCP_APP_NAME} -o jsonpath='{.spec.template.spec.containers[0].image}' --token=${OCP_SERVICE_TOKEN} $target_cluster_flags",
                                             returnStdout: true
                                         )
                                     //if current DeploymentConfig image tag version it's different form BUIL_TAG we end the pipeline with an error
-                                    if (!currentImageStreamVersion.equalsIgnoreCase("$docker_registry:5000/${OCP_PRJ_NAMESPACE}/${OCP_BUILD_NAME}:${BUILD_TAG}")) {
+                                    if (!currentImageStreamVersion.equalsIgnoreCase("$docker_registry:5000/${OCP_PROJECT}/${OCP_APP_NAME}:${BUILD_TAG}")) {
                                         echo "DeploymentConfig image tag version is: $currentImageStreamVersion but expected tag is ${BUILD_TAG}"
                                         currentBuild.result = 'ERROR'
                                         error('Rollout finished with errors: DeploymentConfig image tag version is wrong')
                                     }
 
                                 }
-                                echo "Patch imageStream result: $patchIamgeStream"
+                                echo "Patch imageStream result: $patchImageStream"
                             }
                         }
                     }
@@ -165,7 +149,7 @@ pipeline {
                             withCredentials([string(credentialsId: "${OCP_SERVICE_TOKEN}", variable: 'OCP_SERVICE_TOKEN')]) {
                                 def rollout =
                                     sh(
-                                        script: "oc rollout latest ${OCP_BUILD_NAME} --token=${OCP_SERVICE_TOKEN} $target_cluster_flags",
+                                        script: "oc rollout latest ${OCP_APP_NAME} --token=${OCP_SERVICE_TOKEN} $target_cluster_flags",
                                         returnStdout: true
                                     )
                                 if (!rollout?.trim()) {
